84dda55e0f30cb0cdc07c300eae9747666cceac8
Rob Buckley <robertbckly@gmail.com>
2025-02-22 14:47:51 +0000

    fix overflow scroll issue

--- src/components/routes/app/item/item-hit-box.tsx
+++ src/components/routes/app/item/item-hit-box.tsx
@@ -1,30 +1,25 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 /**
  * Invisible hit box to extend item surface for pointer events;
  * enables out-of-bounds dragging to be maintained
  */
 export const ItemHitBox = () => {
-  const [leftOffset, setLeftOffset] = useState(0);
   const hitBoxRef = useRef<HTMLDivElement>(null);
 
-  // Update `leftOffset` on window resize
+  // Dynamically update width and offset if window resized
   useEffect(() => {
     const updateOffset = () => {
-      const parent = hitBoxRef.current?.parentElement;
-      setLeftOffset(parent?.getBoundingClientRect().x || 0);
+      const hitBox = hitBoxRef.current;
+      const parent = hitBox?.parentElement;
+      if (!hitBox || !parent) return;
+      hitBox.style.left = `-${parent.getBoundingClientRect().x || 0}px`;
+      hitBox.style.width = `${document.body.clientWidth}px`;
     };
     updateOffset();
     window.addEventListener('resize', updateOffset);
     return () => window.addEventListener('resize', updateOffset);
   }, []);
 
-  return (
-    <div
-      ref={hitBoxRef}
-      style={{ left: `-${leftOffset}px` }}
-      // As tall as item & as wide as viewport
-      className="absolute z-[1] h-full w-dvw"
-    />
-  );
+  return <div ref={hitBoxRef} className="absolute z-[1] h-full" />;
 };
