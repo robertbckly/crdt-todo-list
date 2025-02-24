b2219758f1c5ba1907226863720c1e8e21097fb5
Rob Buckley <robertbckly@gmail.com>
2025-02-24 22:43:56 +0000

    improve

--- src/utils/auto-scroll.ts
+++ src/utils/auto-scroll.ts
@@ -4,10 +4,9 @@ let currentScroll: 'up' | 'down' | null = null;
 
 // TODO: add timer-based incrementing for inter-device consistency
 const autoScroll = () => {
-  if (currentScroll !== null) {
-    window.scrollBy({ top: currentScroll === 'up' ? -RATE : RATE });
-    requestAnimationFrame(autoScroll);
-  }
+  if (currentScroll === null) return;
+  window.scrollBy({ top: currentScroll === 'up' ? -RATE : RATE });
+  requestAnimationFrame(autoScroll);
 };
 
 export const startAutoScroll = (type: Exclude<typeof currentScroll, null>) => {
