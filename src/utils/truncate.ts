8f620819601611dd47505fde634326d91e73a346
Rob Buckley <robertbckly@gmail.com>
2025-04-23 18:29:42 +0100

    improve truncate fn

--- src/utils/truncate.ts
+++ src/utils/truncate.ts
@@ -1,4 +1,18 @@
-export const truncate = (string: string, length: number) => {
-  const shouldShowEllipsis = string.length > length;
-  return `${string.substring(0, length)}${shouldShowEllipsis ? '...' : ''}`;
+export const truncate = (
+  string: string,
+  length: number,
+  stopBeforeBreak: boolean = true,
+) => {
+  const substring = string.trim().substring(0, length);
+  const substringLines = substring.split('\n');
+
+  const hasBreaks = substringLines.length > 1;
+  const shouldShowEllipsis =
+    substring.length < string.length || (hasBreaks && stopBeforeBreak);
+
+  if (stopBeforeBreak && shouldShowEllipsis) {
+    return `${substringLines[0] || ''}...`;
+  }
+
+  return `${substring}${shouldShowEllipsis ? '...' : ''}`;
 };
